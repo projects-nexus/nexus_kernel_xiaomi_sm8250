a897d31d69e7ade6a5702c01e8d24846a8431238
John Stultz <john.stultz@linaro.org>
2011-12-15 18:51:04 -0800

    ANDROID: ashmem: Add shmem_set_file to mm/shmem.c
    NOT FOR STAGING
    This patch re-adds the original shmem_set_file to mm/shmem.c
    and converts ashmem.c back to using it.
    
    Change-Id: Ie604c9f8f4d0ee6bc2aae1a96d261c8373a1a2dc
    CC: Brian Swetland <swetland@google.com>
    CC: Colin Cross <ccross@android.com>
    CC: Arve Hjønnevåg <arve@android.com>
    CC: Dima Zavin <dima@android.com>
    CC: Robert Love <rlove@google.com>
    CC: Greg KH <greg@kroah.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    [Sultan Alsawaf: forward port to 5.10]
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/staging/android/ashmem.c
+++ drivers/staging/android/ashmem.c
@@ -440,18 +440,13 @@ static int ashmem_mmap(struct file *file, struct vm_area_struct *vma)
 	 * shmem_set_file while we're in staging. -jstultz
 	 */
 	if (vma->vm_flags & VM_SHARED) {
-		ret = shmem_zero_setup(vma);
-		if (ret) {
-			fput(asma->file);
-			goto out;
-		}
+		shmem_set_file(vma, asma->file);
 	} else {
 		vma_set_anonymous(vma);
+		if (vma->vm_file)
+			fput(vma->vm_file);
+		vma->vm_file = asma->file;
 	}
-
-	if (vma->vm_file)
-		fput(vma->vm_file);
-	vma->vm_file = asma->file;
 
 out:
 	mutex_unlock(&ashmem_mutex);
