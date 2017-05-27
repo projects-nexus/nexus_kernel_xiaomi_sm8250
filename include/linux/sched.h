4ad1bf9021c833dc8d28fe4d9c9d6ad4007a0a86
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2017-05-27 19:02:06 +0200

    kernel/sched/core: add migrate_disable()
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -919,6 +919,13 @@ struct task_struct {
 	cpumask_t			cpus_requested;
 	const cpumask_t			*cpus_ptr;
 	cpumask_t			cpus_mask;
+#if defined(CONFIG_PREEMPT_COUNT) && defined(CONFIG_SMP)
+	int				migrate_disable;
+	int				migrate_disable_update;
+# ifdef CONFIG_SCHED_DEBUG
+	int				migrate_disable_atomic;
+# endif
+#endif
 
 #ifdef CONFIG_PREEMPT_RCU
 	int				rcu_read_lock_nesting;
