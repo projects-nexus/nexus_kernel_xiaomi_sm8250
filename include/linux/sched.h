17e0cb1679c89f23f702027ba1dea00a7b15df01
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2018-07-05 14:44:51 +0200

    sched/migrate_disable: fallback to preempt_disable() instead barrier()
    On SMP + !RT migrate_disable() is still around. It is not part of spin_lock()
    anymore so it has almost no users. However the futex code has a workaround for
    the !in_atomic() part of migrate disable which fails because the matching
    migrade_disable() is no longer part of spin_lock().
    
    On !SMP + !RT migrate_disable() is reduced to barrier(). This is not optimal
    because we few spots where a "preempt_disable()" statement was replaced with
    "migrate_disable()".
    
    We also used the migration_disable counter to figure out if a sleeping lock is
    acquired so RCU does not complain about schedule() during rcu_read_lock() while
    a sleeping lock is held. This changed, we no longer use it, we have now a
    sleeping_lock counter for the RCU purpose.
    
    This means we can now:
    - for SMP + RT_BASE
      full migration program, nothing changes here
    
    - for !SMP + RT_BASE
      the migration counting is no longer required. It used to ensure that the task
      is not migrated to another CPU and that this CPU remains online. !SMP ensures
      that already.
      Move it to CONFIG_SCHED_DEBUG so the counting is done for debugging purpose
      only.
    
    - for all other cases including !RT
      fallback to preempt_disable(). The only remaining users of migrate_disable()
      are those which were converted from preempt_disable() and the futex
      workaround which is already in the preempt_disable() section due to the
      spin_lock that is held.
    
    Cc: stable-rt@vger.kernel.org
    Reported-by: joe.korty@concurrent-rt.com
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -924,7 +924,7 @@ struct task_struct {
 	cpumask_t			cpus_requested;
 	const cpumask_t			*cpus_ptr;
 	cpumask_t			cpus_mask;
-#if defined(CONFIG_PREEMPT_COUNT) && defined(CONFIG_SMP)
+#if defined(CONFIG_SMP) && defined(CONFIG_PREEMPT_RT_BASE)
 	int				migrate_disable;
 	int				migrate_disable_update;
 # ifdef CONFIG_SCHED_DEBUG
@@ -932,8 +932,8 @@ struct task_struct {
 # endif
 
 #elif !defined(CONFIG_SMP) && defined(CONFIG_PREEMPT_RT_BASE)
-	int				migrate_disable;
 # ifdef CONFIG_SCHED_DEBUG
+	int				migrate_disable;
 	int				migrate_disable_atomic;
 # endif
 #endif
