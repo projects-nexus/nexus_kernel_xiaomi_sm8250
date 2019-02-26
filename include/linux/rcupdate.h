b4ceb61342e27cc71b0bbecf1cc9cf1b82231c68
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2019-02-26 16:56:02 +0100

    rbtree: don't include the rcu header
    The RCU header pulls in spinlock.h and fails due not yet defined types:
    
    |In file included from include/linux/spinlock.h:275:0,
    |                 from include/linux/rcupdate.h:38,
    |                 from include/linux/rbtree.h:34,
    |                 from include/linux/rtmutex.h:17,
    |                 from include/linux/spinlock_types.h:18,
    |                 from kernel/bounds.c:13:
    |include/linux/rwlock_rt.h:16:38: error: unknown type name ‘rwlock_t’
    | extern void __lockfunc rt_write_lock(rwlock_t *rwlock);
    |                                      ^
    
    This patch moves the required RCU function from the rcupdate.h header file into
    a new header file which can be included by both users.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -42,6 +42,7 @@
 #include <linux/lockdep.h>
 #include <asm/processor.h>
 #include <linux/cpumask.h>
+#include <linux/rcu_assign_pointer.h>
 
 #define ULONG_CMP_GE(a, b)	(ULONG_MAX / 2 >= (a) - (b))
 #define ULONG_CMP_LT(a, b)	(ULONG_MAX / 2 < (a) - (b))
