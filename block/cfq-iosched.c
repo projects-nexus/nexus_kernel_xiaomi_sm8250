3f81084b7d7e9393af2e78f6c6d7b4baa7d4b7a5
celtare21 <celtare21@gmail.com>
2019-12-18 11:24:44 +0000

    ANDROID: block: cfq: Disable logging if trace is not enabled
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: atndko <z1281552865@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -665,6 +665,8 @@ static inline void cfqg_put(struct cfq_group *cfqg)
 }
 
 #define cfq_log_cfqq(cfqd, cfqq, fmt, args...)	do {			\
+	if (likely(!blk_trace_note_message_enabled((cfqd)->queue)))	\
+		break;							\
 	blk_add_cgroup_trace_msg((cfqd)->queue,				\
 			cfqg_to_blkg((cfqq)->cfqg)->blkcg,		\
 			"cfq%d%c%c " fmt, (cfqq)->pid,			\
@@ -792,6 +794,8 @@ static inline void cfqg_get(struct cfq_group *cfqg) { }
 static inline void cfqg_put(struct cfq_group *cfqg) { }
 
 #define cfq_log_cfqq(cfqd, cfqq, fmt, args...)	\
+	if (likely(!blk_trace_note_message_enabled((cfqd)->queue)))	\
+		break;							\
 	blk_add_trace_msg((cfqd)->queue, "cfq%d%c%c " fmt, (cfqq)->pid,	\
 			cfq_cfqq_sync((cfqq)) ? 'S' : 'A',		\
 			cfqq_type((cfqq)) == SYNC_NOIDLE_WORKLOAD ? 'N' : ' ',\
