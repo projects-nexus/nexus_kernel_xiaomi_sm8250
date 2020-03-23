b1b658ec6d9a94b7c6254f1943dbee8f2667f8f0
xNombre <kartapolska@gmail.com>
2020-03-24 00:19:27 +0100

    block: Completely disable readahead
    ...and restrict if from being changed by userspace.
    
    Remove deprecated flag while at it.
    
    PCMark Storage bench showed ~130 points increase in performance.
    It will also save much power.
    
    @THEBOSS619: To Include more evidences...
    
    [*] https://twitter.com/arter97/status/1003054292897370112
    [*] https://www.semanticscholar.org/paper/Revisiting-read-ahead-efficiency-for-raw-NAND-flash-Olivier-Boukhobza/297f3d189dcd9b099f8f6b5fbc7e304db395a683
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -100,16 +100,7 @@ static ssize_t queue_ra_show(struct request_queue *q, char *page)
 static ssize_t
 queue_ra_store(struct request_queue *q, const char *page, size_t count)
 {
-	unsigned long ra_kb;
-	ssize_t ret = queue_var_store(&ra_kb, page, count);
-
-	if (ret < 0)
-		return ret;
-
-	ra_kb = 128;
-	q->backing_dev_info->ra_pages = ra_kb >> (PAGE_SHIFT - 10);
-
-	return ret;
+	return count;
 }
 
 static ssize_t queue_max_sectors_show(struct request_queue *q, char *page)
