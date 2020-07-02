f3e464b7e5ac09ba873946aae59959913df6a277
Rick Yiu <rickyiu@google.com>
2020-07-02 22:21:55 +0800

    sched: refine code for computing energy
    When computing energy of each cpu, we should simulate the boost
    margin if the task is enqueued on that cpu. This is to sync with
    ACK change - aosp/1346105.
    
    Bug: 158637636
    Test: build pass
    Change-Id: If5cbec9ac04fea46830c32f797c6b09dce1ea1a2
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    
    [hridaya: Fix conflict with commit 3965f10a4adf7c22adc2bd0ddfa6aed7eedd62da
    	("ANDROID: sched: consider stune boost margin when computing energy")
    	which is ACK upstream.]
    
    Change-Id: Iab65990d78fc611e3ec2e6959380c1cc0c57472f

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6243,6 +6243,7 @@ long schedtune_task_margin(struct task_struct *task)
 	return margin;
 }
 
+#ifdef CONFIG_SCHED_WALT
 unsigned long
 stune_util(int cpu, unsigned long other_util,
 		 struct sched_walt_cpu_load *walt_load)
@@ -6258,6 +6259,7 @@ stune_util(int cpu, unsigned long other_util,
 	else
 		return util;
 }
+#endif
 
 #else /* CONFIG_SCHED_TUNE */
 
