e52b1924f8f8fc327c2c82e677c9dda1be27ed26
Park Ju Hyung <qkrwngud825@gmail.com>
2020-10-15 23:14:24 +0900

    sysctl: rename /proc/sys/vm/swappiness to vbswappiness
    This kernel handles swappiness differently, and OxygenOS was
    overriding it to 100.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    [@0ctobot: Refactor for clarity]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1768,7 +1768,11 @@ static struct ctl_table vm_table[] = {
 		.extra1		= &zero,
 	},
 	{
+#ifdef CONFIG_VBSWAP
+		.procname	= "vbswappiness",
+#else
 		.procname	= "rswappiness",
+#endif
 		.data		= &vm_swappiness,
 		.maxlen		= sizeof(vm_swappiness),
 		.mode		= 0644,
