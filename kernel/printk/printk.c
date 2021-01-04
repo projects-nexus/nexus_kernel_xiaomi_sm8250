71918becf3cfe0632b801d2a1744429ffa039624
wloot <wloot@qq.com>
2021-01-04 17:30:15 +0200

    printk: silence healthd and batteryd logs
    * <3>batteryd, <4>healthd

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -820,6 +820,10 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 	 */
 	line = buf;
 	if (line[0] == '<') {
+		if (memcmp(line+3, "batteryd", sizeof("batteryd")-1) == 0 ||
+			   memcmp(line+3, "healthd", sizeof("healthd")-1) == 0)
+			goto free;
+		{
 		char *endp = NULL;
 		unsigned int u;
 
@@ -834,6 +838,7 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			if (strstr(line, "healthd") ||
 				strstr(line, "cacert"))
 				goto free;
+		}
 		}
 	}
 
