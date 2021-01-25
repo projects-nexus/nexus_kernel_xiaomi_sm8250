9b29f21e6e743566e04d8fd3b82885b2c5e24876
Mel Gorman <mgorman@techsingularity.net>
2021-01-25 08:59:07 +0000

    sched/fair: Move avg_scan_cost calculations under SIS_PROP
    As noted by Vincent Guittot, avg_scan_costs are calculated for SIS_PROP
    even if SIS_PROP is disabled. Move the time calculations under a SIS_PROP
    check and while we are at it, exclude the cost of initialising the CPU
    mask from the average scan cost.
    
    Change-Id: I19a4b42a62524815af942ff0288b989075dcf3c7
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20210125085909.4600-3-mgorman@techsingularity.net

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6668,6 +6668,8 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 	if (!this_sd)
 		return -1;
 
+	cpumask_and(cpus, sched_domain_span(sd), &p->cpus_allowed);
+
 	if (sched_feat(SIS_PROP)) {
 		u64 avg_cost, avg_idle, span_avg;
 
@@ -6683,11 +6685,9 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 			nr = div_u64(span_avg, avg_cost);
 		else
 			nr = 4;
+
+		time = local_clock();
 	}
-
-	time = local_clock();
-
-	cpumask_and(cpus, sched_domain_span(sd), &p->cpus_allowed);
 
 	for_each_cpu_wrap(cpu, cpus, target) {
 		if (!--nr)
@@ -6698,10 +6698,12 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 			break;
 	}
 
-	time = local_clock() - time;
-	cost = this_sd->avg_scan_cost;
-	delta = (s64)(time - cost) / 8;
-	this_sd->avg_scan_cost += delta;
+	if (sched_feat(SIS_PROP)) {
+		time = local_clock() - time;
+		cost = this_sd->avg_scan_cost;
+		delta = (s64)(time - cost) / 8;
+		this_sd->avg_scan_cost += delta;
+	}
 
 	return cpu;
 }
