7dd4247b593654076a7cda53c083dc5d93bad601
Christoph Hellwig <hch@lst.de>
2021-02-02 13:13:27 +0100

    BACKPORT: kallsyms: only build {,module_}kallsyms_on_each_symbol when required
    kallsyms_on_each_symbol and module_kallsyms_on_each_symbol are only used
    by the livepatching code, so don't build them if livepatching is not
    enabled.
    
    Reviewed-by: Miroslav Benes <mbenes@suse.cz>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jessica Yu <jeyu@kernel.org>
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -4173,6 +4173,7 @@ unsigned long module_kallsyms_lookup_name(const char *name)
 	return ret;
 }
 
+#ifdef CONFIG_LIVEPATCH
 int module_kallsyms_on_each_symbol(int (*fn)(void *, const char *,
 					     struct module *, unsigned long),
 				   void *data)
@@ -4202,6 +4203,7 @@ int module_kallsyms_on_each_symbol(int (*fn)(void *, const char *,
 	mutex_unlock(&module_mutex);
 	return ret;
 }
+#endif /* CONFIG_LIVEPATCH */
 #endif /* CONFIG_KALLSYMS */
 
 static void cfi_init(struct module *mod)
