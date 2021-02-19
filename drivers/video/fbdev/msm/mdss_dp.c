6485689ed2ab402105be95faeaeb362bfb53751d
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-02-19 08:23:05 +0700

    treewide: silence some logger
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/video/fbdev/msm/mdss_dp.c
+++ drivers/video/fbdev/msm/mdss_dp.c
@@ -4465,7 +4465,7 @@ static int mdss_dp_usbpd_setup(struct mdss_dp_drv_pdata *dp_drv)
 						    pd_phandle);
 
 	if (IS_ERR(dp_drv->pd)) {
-		pr_err("get_usbpd phandle failed (%ld)\n",
+		pr_debug("get_usbpd phandle failed (%ld)\n",
 				PTR_ERR(dp_drv->pd));
 		return PTR_ERR(dp_drv->pd);
 	}
@@ -4539,7 +4539,7 @@ static int mdss_dp_probe(struct platform_device *pdev)
 	init_completion(&dp_drv->audio_comp);
 
 	if (mdss_dp_usbpd_setup(dp_drv)) {
-		pr_err("Error usbpd setup!\n");
+		pr_debug("Error usbpd setup!\n");
 		dp_drv = NULL;
 		return -EPROBE_DEFER;
 	}
--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -511,7 +511,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 	suspend_test_start();
 	error = dpm_suspend_start(PMSG_SUSPEND);
 	if (error) {
-		pr_err("Some devices failed to suspend, or early wake event detected\n");
+		pr_debug("Some devices failed to suspend, or early wake event detected\n");
 		log_suspend_abort_reason(
 				"Some devices failed to suspend, or early wake event detected");
 		goto Recover_platform;
