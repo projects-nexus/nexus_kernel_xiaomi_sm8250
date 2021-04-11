9d1bdb3fe5b11d5b1f5c0db4a7fbe80e098268c8
Diep Quynh <remilia.1505@gmail.com>
2021-04-12 03:35:22 +0700

    sched: Cover more WALT balancing cases
    These pieces of codes are only useful for WALT.
    
    Co-authored-by: EmanuelCN <emanuelghub@gmail.com>
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4018,7 +4018,9 @@ void scheduler_tick(void)
 	u64 wallclock;
 	bool early_notif;
 	u32 old_load;
+#ifdef CONFIG_SCHED_WALT
 	struct related_thread_group *grp;
+#endif
 	unsigned int flag = 0;
 
 	sched_clock_tick();
@@ -4049,6 +4051,7 @@ void scheduler_tick(void)
 	trigger_load_balance(rq);
 #endif
 
+#ifdef CONFIG_SCHED_WALT
 	rcu_read_lock();
 	grp = task_related_thread_group(curr);
 	if (update_preferred_cluster(grp, curr, old_load, true))
@@ -4057,6 +4060,7 @@ void scheduler_tick(void)
 
 	if (curr->sched_class == &fair_sched_class)
 		check_for_migration(rq, curr);
+#endif
 
 #ifdef CONFIG_SMP
 	rq_lock(rq, &rf);
@@ -4064,7 +4068,6 @@ void scheduler_tick(void)
 		clear_reserved(cpu);
 	rq_unlock(rq, &rf);
 #endif
-
 }
 
 #ifdef CONFIG_NO_HZ_FULL
