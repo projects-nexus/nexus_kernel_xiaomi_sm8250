e36d6bca198393415073f062641fbc1fc83d9560
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2021-04-22 19:41:24 +0000

    drivers: input: touchscreen: Prevent unnecessary input sync
    * Disable the IRQ boost for touch as this only causes garbage data to be spammed & disable accidental touches as this doesn't work properly on AOSP & takes false positives causing alot of issues like lack of inertia in chrome scrolling & gestures not working correctly on launcher
    
    Change-Id: I83591a483f4a10aa57c27692acc388a0618ffef9
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/input/touchscreen/xiaomi/xiaomi_touch.c
+++ drivers/input/touchscreen/xiaomi/xiaomi_touch.c
@@ -534,6 +534,7 @@ static int xiaomi_touch_parse_dt(struct device *dev,
 	return 0;
 }
 
+#if XIAOMI_ROI
 void xiaomi_touch_send_btn_tap_key(int status)
 {
 	if (xiaomi_touch_dev.key_input_dev) {
@@ -550,6 +551,7 @@ void xiaomi_touch_send_btn_tap_key(int status)
 	}
 }
 EXPORT_SYMBOL(xiaomi_touch_send_btn_tap_key);
+#endif
 
 static int xiaomi_touch_probe(struct platform_device *pdev)
 {
