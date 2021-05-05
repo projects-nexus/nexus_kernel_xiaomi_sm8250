87384e38fab800d4f2a1d67809b7726dcdd92d3d
Yang Shi <shy828301@gmail.com>
2021-05-04 18:36:33 -0700

    mm: vmscan: add per memcg shrinker nr_deferred
    Currently the number of deferred objects are per shrinker, but some
    slabs, for example, vfs inode/dentry cache are per memcg, this would
    result in poor isolation among memcgs.
    
    The deferred objects typically are generated by __GFP_NOFS allocations,
    one memcg with excessive __GFP_NOFS allocations may blow up deferred
    objects, then other innocent memcgs may suffer from over shrink,
    excessive reclaim latency, etc.
    
    For example, two workloads run in memcgA and memcgB respectively,
    workload in B is vfs heavy workload.  Workload in A generates excessive
    deferred objects, then B's vfs cache might be hit heavily (drop half of
    caches) by B's limit reclaim or global reclaim.
    
    We observed this hit in our production environment which was running vfs
    heavy workload shown as the below tracing log:
    
      <...>-409454 [016] .... 28286961.747146: mm_shrink_slab_start: super_cache_scan+0x0/0x1a0 ffff9a83046f3458:
      nid: 1 objects to shrink 3641681686040 gfp_flags GFP_HIGHUSER_MOVABLE|__GFP_ZERO pgs_scanned 1 lru_pgs 15721
      cache items 246404277 delta 31345 total_scan 123202138
      <...>-409454 [022] .... 28287105.928018: mm_shrink_slab_end: super_cache_scan+0x0/0x1a0 ffff9a83046f3458:
      nid: 1 unused scan count 3641681686040 new scan count 3641798379189 total_scan 602
      last shrinker return val 123186855
    
    The vfs cache and page cache ratio was 10:1 on this machine, and half of
    caches were dropped.  This also resulted in significant amount of page
    caches were dropped due to inodes eviction.
    
    Make nr_deferred per memcg for memcg aware shrinkers would solve the
    unfairness and bring better isolation.
    
    The following patch will add nr_deferred to parent memcg when memcg
    offline.  To preserve nr_deferred when reparenting memcgs to root, root
    memcg needs shrinker_info allocated too.
    
    When memcg is not enabled (!CONFIG_MEMCG or memcg disabled), the
    shrinker's nr_deferred would be used.  And non memcg aware shrinkers use
    shrinker's nr_deferred all the time.
    
    Link: https://lkml.kernel.org/r/20210311190845.9708-10-shy828301@gmail.com
    Signed-off-by: Yang Shi <shy828301@gmail.com>
    Acked-by: Roman Gushchin <guro@fb.com>
    Acked-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Reviewed-by: Shakeel Butt <shakeelb@google.com>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -112,12 +112,13 @@ struct lruvec_stat {
 };
 
 /*
- * Bitmap of shrinker::id corresponding to memcg-aware shrinkers,
- * which have elements charged to this memcg.
+ * Bitmap and deferred work of shrinker::id corresponding to memcg-aware
+ * shrinkers, which have elements charged to this memcg.
  */
 struct shrinker_info {
 	struct rcu_head rcu;
-	unsigned long map[0];
+	atomic_long_t *nr_deferred;
+	unsigned long *map;
 };
 
 /*
