7a8abfcbf9fd082dc1dca83e337c9e84a6cdc59a
Yang Shi <shy828301@gmail.com>
2021-05-04 18:36:29 -0700

    mm: vmscan: use a new flag to indicate shrinker is registered
    Currently registered shrinker is indicated by non-NULL
    shrinker->nr_deferred.  This approach is fine with nr_deferred at the
    shrinker level, but the following patches will move MEMCG_AWARE
    shrinkers' nr_deferred to memcg level, so their shrinker->nr_deferred
    would always be NULL.  This would prevent the shrinkers from
    unregistering correctly.
    
    Remove SHRINKER_REGISTERING since we could check if shrinker is
    registered successfully by the new flag.
    
    Link: https://lkml.kernel.org/r/20210311190845.9708-9-shy828301@gmail.com
    Signed-off-by: Yang Shi <shy828301@gmail.com>
    Acked-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Acked-by: Roman Gushchin <guro@fb.com>
    Reviewed-by: Shakeel Butt <shakeelb@google.com>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/shrinker.h
+++ include/linux/shrinker.h
@@ -79,8 +79,9 @@ struct shrinker {
 #define DEFAULT_SEEKS 2 /* A good number if you don't know better. */
 
 /* Flags */
-#define SHRINKER_NUMA_AWARE	(1 << 0)
-#define SHRINKER_MEMCG_AWARE	(1 << 1)
+#define SHRINKER_REGISTERED	(1 << 0)
+#define SHRINKER_NUMA_AWARE	(1 << 1)
+#define SHRINKER_MEMCG_AWARE	(1 << 2)
 
 extern int prealloc_shrinker(struct shrinker *shrinker);
 extern void register_shrinker_prepared(struct shrinker *shrinker);
