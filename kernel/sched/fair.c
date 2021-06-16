70b926fe8d7736c64775b919de8f6e8e686a2eb6
Rick Yiu <rickyiu@google.com>
2021-06-16 16:12:50 +0800

    sched/fair: Fix kernel warning
    Fix idle_get_state_idx() callded without holding rcu read lock.
    
    Bug: 189064175
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I719b2917ad45a9a03e8f2aa16f6a4cb356c117be
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8085,8 +8085,6 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu,
 		}
 	}
 unlock:
-	rcu_read_unlock();
-
 	/*
 	 * Pick the prev CPU, if best energy CPU can't saves at least 6% of
 	 * the energy used by prev_cpu.
@@ -8096,6 +8094,8 @@ unlock:
 	    (prev_energy != ULONG_MAX) && (best_energy_cpu != prev_cpu) &&
 	    ((prev_energy - best_energy) <= prev_energy >> 4))
 		best_energy_cpu = prev_cpu;
+
+	rcu_read_unlock();
 
 done:
 
