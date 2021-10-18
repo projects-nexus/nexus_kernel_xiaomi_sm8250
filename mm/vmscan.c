8680ba25fa3f0eed9dda700bb428d02189ecccf5
Johannes Weiner <hannes@cmpxchg.org>
2021-10-18 12:23:34 +0200

    mm: vmscan: fix missing psi annotation for node_reclaim()
    In a debugging session the other day, Rik noticed that node_reclaim()
    was missing memstall annotations.  This means we'll miss pressure and
    lost productivity resulting from reclaim on an overloaded local NUMA
    node when vm.zone_reclaim_mode is enabled.
    
    There haven't been any reports, but that's likely because
    vm.zone_reclaim_mode hasn't been a commonly used feature recently, and
    the intersection between such setups and psi users is probably nil.
    
    But secondary memory such as CXL-connected DIMMS, persistent memory etc,
    and the page demotion patches that handle them
    (https://lore.kernel.org/lkml/20210401183216.443C4443@viggo.jf.intel.com/)
    could soon make this a more common codepath again.
    
    Link: https://lkml.kernel.org/r/20210818152457.35846-1-hannes@cmpxchg.org
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Reported-by: Rik van Riel <riel@surriel.com>
    Reviewed-by: Shakeel Butt <shakeelb@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Change-Id: I8459dd36489c3266c48943da8f83cb00acce722d
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4350,9 +4350,11 @@ static int __node_reclaim(struct pglist_data *pgdat, gfp_t gfp_mask, unsigned in
 		.may_swap = 1,
 		.reclaim_idx = gfp_zone(gfp_mask),
 	};
+	unsigned long pflags;
 
 	cond_resched();
 	fs_reclaim_acquire(sc.gfp_mask);
+	psi_memstall_enter(&pflags);
 	/*
 	 * We need to be able to allocate from the reserves for RECLAIM_UNMAP
 	 * and we also need to be able to write out pages for RECLAIM_WRITE
@@ -4377,6 +4379,8 @@ static int __node_reclaim(struct pglist_data *pgdat, gfp_t gfp_mask, unsigned in
 	current->flags &= ~PF_SWAPWRITE;
 	memalloc_noreclaim_restore(noreclaim_flag);
 	fs_reclaim_release(sc.gfp_mask);
+	psi_memstall_leave(&pflags);
+
 	return sc.nr_reclaimed >= nr_pages;
 }
 
