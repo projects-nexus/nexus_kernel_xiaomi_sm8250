d84b32560459dce2a660319de493f64e6688b93b
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2021-11-02 09:13:45 +0600

    schedhorizon: Add configuration options for efficient_freq/up_delay
    This adds configuration options for default efficient freq (lp & hp)
    and default up delay (lp & hp) at drivers/cpufreq/Kconfig and can be
    configured from the device specific defconfigs.
    
    up_delay will use a multiplier "NSEC_PER_MSEC" from now on where the
    Kconfig's default value will be used as a multiplicand at
    kernel/sched/cpufreq_schedhorizon.c .
    
    [NotZeetaa: Added prime cluster also adapted to our cpu frequencys]
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kernel/sched/cpufreq_schedhorizon.c
+++ kernel/sched/cpufreq_schedhorizon.c
@@ -17,14 +17,14 @@
 #include <trace/events/power.h>
 #include <linux/sched/sysctl.h>
 
-static unsigned int default_efficient_freq_lp[] = {0};
-static u64 default_up_delay_lp[] = {0};
+static unsigned int default_efficient_freq_lp[] = {CONFIG_SCHEDHORIZON_DEFAULT_EFFICIENT_FREQ_LP};
+static u64 default_up_delay_lp[] = {CONFIG_SCHEDHORIZON_DEFAULT_UP_DELAY_LP * NSEC_PER_MSEC};
 
-static unsigned int default_efficient_freq_hp[] = {1766400};
-static u64 default_up_delay_hp[] = {100 * NSEC_PER_MSEC};
+static unsigned int default_efficient_freq_hp[] = {CONFIG_SCHEDHORIZON_DEFAULT_EFFICIENT_FREQ_HP};
+static u64 default_up_delay_hp[] = {CONFIG_SCHEDHORIZON_DEFAULT_UP_DELAY_HP * NSEC_PER_MSEC};
 
-static unsigned int default_efficient_freq_pr[] = {2073600};
-static u64 default_up_delay_pr[] = {100 * NSEC_PER_MSEC};
+static unsigned int default_efficient_freq_pr[] = {CONFIG_SCHEDHORIZON_DEFAULT_EFFICIENT_FREQ_PR};
+static u64 default_up_delay_pr[] = {CONFIG_SCHEDHORIZON_DEFAULT_UP_DELAY_PR * NSEC_PER_MSEC};
 
 #define DEFAULT_RTG_BOOST_FREQ_LP 0
 #define DEFAULT_RTG_BOOST_FREQ_HP 0
