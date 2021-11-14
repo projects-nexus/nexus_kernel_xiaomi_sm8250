d400ca0add2220c556a6306f02388e8b9310185a
Peter Zijlstra <peterz@infradead.org>
2021-11-14 20:14:06 +0100

    UPSTREAM: sched,fair: Alternative sched_slice()
    The current sched_slice() seems to have issues; there's two possible
    things that could be improved:
    
     - the 'nr_running' used for __sched_period() is daft when cgroups are
       considered. Using the RQ wide h_nr_running seems like a much more
       consistent number.
    
     - (esp) cgroups can slice it real fine, which makes for easy
       over-scheduling, ensure min_gran is what the name says.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Valentin Schneider <valentin.schneider@arm.com>
    Link: https://lkml.kernel.org/r/20210412102001.611897312@infradead.org
    Change-Id: Ieb47e717679de51e632b9814ef7d1e34ae0ae256
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -124,6 +124,9 @@
  */
 #define SCHED_FEAT_SCHEDTUNE_BOOST_HOLD_ALL 0
 
+#define SCHED_FEAT_ALT_PERIOD 1
+#define SCHED_FEAT_BASE_SLICE 1
+
 /*
  * Inflate the effective utilization of SchedTune-boosted tasks, which
  * generally leads to usage of higher frequencies.
