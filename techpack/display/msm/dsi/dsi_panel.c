c1ac9968996543a74a66300673f50974595b315e
Arian <arian.kulmer@web.de>
2021-11-21 17:31:26 -0600

    techpack: display: Skip fod_hbm while HBM is active
    Change-Id: I84409400b4ba33370a32e8e9cbe87b3ad9dfb83c
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -840,6 +840,9 @@ static u32 interpolate(uint32_t x, uint32_t xa, uint32_t xb, uint32_t ya, uint32
 
 u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel)
 {
+	if (panel->hbm_mode)
+		return 0;
+
 	u32 brightness = dsi_panel_get_backlight(panel);
 	int i;
 
@@ -864,6 +867,9 @@ u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel)
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 {
 	int rc = 0;
+	
+	if (panel->hbm_mode)
+		return rc;
 
 	if (status) {
 		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_MI_HBM_FOD_ON);
