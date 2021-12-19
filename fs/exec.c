1e6280d97b47963aba35734b61d34eb943c066f7
NotZeetaa <rodrigo2005contente@gmail.com>
2021-12-19 20:42:33 +0000

    fs: exec: Fix derp commit
    https://github.com/NotZeetaa/android_kernel_xiaomi_sm8250/commit/77ef56a552407c2c7be0b9eb48a2d0c52c4b2b29
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1852,20 +1852,21 @@ static int __do_execve_file(int fd, struct filename *filename,
 		else if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
-			current->pc_flags |= PC_PRIME_CRITICAL;
+			current->pc_flags |= PC_PRIME_AFFINE;
 			set_cpus_allowed_ptr(current, cpu_prime_mask);
 		}
 		else if (unlikely(!strncmp(filename->name,
 					   UDFPS_BIN_PREFIX,
 					   strlen(UDFPS_BIN_PREFIX)))) {
-		        current->pc_flags |= PC_PRIME_CRITICAL;
+		        current->pc_flags |= PC_PRIME_AFFINE;
 		        set_cpus_allowed_ptr(current, cpu_prime_mask);
 		}
 		else if (unlikely(!strncmp(filename->name,
 					   FOD_BIN_PREFIX,
 					   strlen(FOD_BIN_PREFIX)))) {
-		        current->pc_flags |= PC_PRIME_CRITICAL;
+		        current->pc_flags |= PC_PRIME_AFFINE;
 		        set_cpus_allowed_ptr(current, cpu_prime_mask);
+		}
 	}
 
 	/* execve succeeded */
