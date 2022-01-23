c697afbc1f295298852d30f73458bdad60e5b5cb
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-01-23 17:57:40 +0600

    net: bonding: Compile out bond_debugfs when !CONFIG_DEBUG_FS
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -3102,7 +3102,9 @@ static int bond_event_changename(struct bonding *bond)
 	bond_remove_proc_entry(bond);
 	bond_create_proc_entry(bond);
 
+#ifdef CONFIG_DEBUG_FS
 	bond_debug_reregister(bond);
+#endif
 
 	return NOTIFY_DONE;
 }
@@ -4422,7 +4424,9 @@ static void bond_uninit(struct net_device *bond_dev)
 
 	list_del(&bond->bond_list);
 
+#ifdef CONFIG_DEBUG_FS
 	bond_debug_unregister(bond);
+#endif
 }
 
 /*------------------------- Module initialization ---------------------------*/
@@ -4831,7 +4835,9 @@ static int bond_init(struct net_device *bond_dev)
 
 	bond_prepare_sysfs_group(bond);
 
+#ifdef CONFIG_DEBUG_FS
 	bond_debug_register(bond);
+#endif
 
 	/* Ensure valid dev_addr */
 	if (is_zero_ether_addr(bond_dev->dev_addr) &&
@@ -4953,7 +4959,9 @@ static int __init bonding_init(void)
 	if (res)
 		goto err_link;
 
+#ifdef CONFIG_DEBUG_FS
 	bond_create_debugfs();
+#endif
 
 	for (i = 0; i < max_bonds; i++) {
 		res = bond_create(&init_net, NULL);
@@ -4965,7 +4973,9 @@ static int __init bonding_init(void)
 out:
 	return res;
 err:
+#ifdef CONFIG_DEBUG_FS
 	bond_destroy_debugfs();
+#endif
 	bond_netlink_fini();
 err_link:
 	unregister_pernet_subsys(&bond_net_ops);
@@ -4977,7 +4987,9 @@ static void __exit bonding_exit(void)
 {
 	unregister_netdevice_notifier(&bond_netdev_notifier);
 
+#ifdef CONFIG_DEBUG_FS
 	bond_destroy_debugfs();
+#endif
 
 	bond_netlink_fini();
 	unregister_pernet_subsys(&bond_net_ops);
