4d92220e902cf56fa4b5cfa3d785241bb07ebbc8
Sultan Alsawaf <sultan@kerneltoast.com>
2022-02-11 10:28:10 -0800

    drm/msm/dsi-staging: Make IRQ handler non-threaded on RT
    The DSI IRQ handler is very time-sensitive and malfunctions often when it's
    threaded on RT, leading to lots of SDE errors and display contents not
    updating on time. Since the IRQ handler needs to run quickly and isn't very
    heavy, make it non-threaded on RT.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- techpack/display/msm/dsi/dsi_ctrl.c
+++ techpack/display/msm/dsi/dsi_ctrl.c
@@ -2187,7 +2187,7 @@ static int dsi_ctrl_dev_probe(struct platform_device *pdev)
 	INIT_WORK(&dsi_ctrl->dma_cmd_wait, dsi_ctrl_dma_cmd_wait_for_done);
 	atomic_set(&dsi_ctrl->dma_irq_trig, 0);
 
-	spin_lock_init(&dsi_ctrl->irq_info.irq_lock);
+	raw_spin_lock_init(&dsi_ctrl->irq_info.irq_lock);
 
 	rc = dsi_ctrl_dts_parse(dsi_ctrl, pdev->dev.of_node);
 	if (rc) {
@@ -2938,9 +2938,9 @@ static irqreturn_t dsi_ctrl_isr(int irq, void *ptr)
 
 	for (i = 0; status && i < DSI_STATUS_INTERRUPT_COUNT; ++i) {
 		if (status & 0x1) {
-			spin_lock_irqsave(&dsi_ctrl->irq_info.irq_lock, flags);
+			raw_spin_lock_irqsave(&dsi_ctrl->irq_info.irq_lock, flags);
 			cb_info = dsi_ctrl->irq_info.irq_stat_cb[i];
-			spin_unlock_irqrestore(
+			raw_spin_unlock_irqrestore(
 					&dsi_ctrl->irq_info.irq_lock, flags);
 
 			if (cb_info.event_cb)
@@ -2981,7 +2981,8 @@ static int _dsi_ctrl_setup_isr(struct dsi_ctrl *dsi_ctrl)
 		rc = irq_num;
 	} else {
 		rc = devm_request_threaded_irq(&dsi_ctrl->pdev->dev, irq_num,
-				dsi_ctrl_isr, NULL, 0, "dsi_ctrl", dsi_ctrl);
+				dsi_ctrl_isr, NULL, IRQF_NO_THREAD, "dsi_ctrl",
+				dsi_ctrl);
 		if (rc) {
 			DSI_CTRL_ERR(dsi_ctrl, "Failed to request IRQ, %d\n",
 					rc);
@@ -3021,7 +3022,7 @@ void dsi_ctrl_enable_status_interrupt(struct dsi_ctrl *dsi_ctrl,
 		return;
 
 	SDE_EVT32(dsi_ctrl->cell_index, SDE_EVTLOG_FUNC_ENTRY);
-	spin_lock_irqsave(&dsi_ctrl->irq_info.irq_lock, flags);
+	raw_spin_lock_irqsave(&dsi_ctrl->irq_info.irq_lock, flags);
 
 	if (dsi_ctrl->irq_info.irq_stat_refcount[intr_idx] == 0) {
 		/* enable irq on first request */
@@ -3042,7 +3043,7 @@ void dsi_ctrl_enable_status_interrupt(struct dsi_ctrl *dsi_ctrl,
 	if (event_info)
 		dsi_ctrl->irq_info.irq_stat_cb[intr_idx] = *event_info;
 
-	spin_unlock_irqrestore(&dsi_ctrl->irq_info.irq_lock, flags);
+	raw_spin_unlock_irqrestore(&dsi_ctrl->irq_info.irq_lock, flags);
 }
 
 void dsi_ctrl_disable_status_interrupt(struct dsi_ctrl *dsi_ctrl,
@@ -3054,7 +3055,7 @@ void dsi_ctrl_disable_status_interrupt(struct dsi_ctrl *dsi_ctrl,
 		return;
 
 	SDE_EVT32(dsi_ctrl->cell_index, SDE_EVTLOG_FUNC_ENTRY);
-	spin_lock_irqsave(&dsi_ctrl->irq_info.irq_lock, flags);
+	raw_spin_lock_irqsave(&dsi_ctrl->irq_info.irq_lock, flags);
 
 	if (dsi_ctrl->irq_info.irq_stat_refcount[intr_idx])
 		if (--(dsi_ctrl->irq_info.irq_stat_refcount[intr_idx]) == 0) {
@@ -3068,7 +3069,7 @@ void dsi_ctrl_disable_status_interrupt(struct dsi_ctrl *dsi_ctrl,
 				disable_irq_nosync(dsi_ctrl->irq_info.irq_num);
 		}
 
-	spin_unlock_irqrestore(&dsi_ctrl->irq_info.irq_lock, flags);
+	raw_spin_unlock_irqrestore(&dsi_ctrl->irq_info.irq_lock, flags);
 }
 
 int dsi_ctrl_host_timing_update(struct dsi_ctrl *dsi_ctrl)
