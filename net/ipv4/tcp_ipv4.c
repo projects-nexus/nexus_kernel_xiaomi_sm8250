c29cbab8842b87b74f818c14918f3818559daca2
NotZeetaa <rodrigo2005contente@gmail.com>
2022-02-19 14:45:15 +0530

    [SQUASH] Revert tcp BACKPORTS
    * This *maybe* can cause drain
    
    This reverts commit 9e61ae3933fb92b96d3d1b8c9e275597f30fac56.
    
    This reverts commit 0b1cd3cc57b3d3656d86999950246f4f317a281f.
    
    This reverts commit df21983f504967b2c3f85d7a79c3f38206548fe2.
    
    This reverts commit 25e936b52afbf3d6f890860080b901d378abc137.
    
    This reverts commit 57785ef724249f7a9bb05ddf765e21f18410aacc.
    
    This reverts commit dec70545abe2ee5cc0d1a61059db229d8c5a2346.
    
    This reverts commit 6e70ed36cc48d90711aac9c962ef3633e8ce360b.
    
    This reverts commit 8913dd7e9b4360b61ccba104d64b8aefeafea89d.
    
    This reverts commit 931a50f75bd9a051c7f76219bff38e68a27321ef.

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1630,13 +1630,13 @@ int tcp_v4_early_demux(struct sk_buff *skb)
 
 bool tcp_add_backlog(struct sock *sk, struct sk_buff *skb)
 {
-        u32 limit;
+	u32 limit = sk->sk_rcvbuf + sk->sk_sndbuf;
 
 	/* Only socket owner can try to collapse/prune rx queues
 	 * to reduce memory overhead, so add a little headroom here.
 	 * Few sockets backlog are possibly concurrently non empty.
 	 */
-	limit = READ_ONCE(sk->sk_rcvbuf) + READ_ONCE(sk->sk_sndbuf) + 64*1024;
+	limit += 64*1024;
 
 	/* In case all data was pulled from skb frags (in __pskb_pull_tail()),
 	 * we can fix skb->truesize to its real value to avoid future drops.
@@ -1825,7 +1825,6 @@ process:
 
 	sk_incoming_cpu_update(sk);
 
-	sk_defer_free_flush(sk);
 	bh_lock_sock_nested(sk);
 	tcp_segs_in(tcp_sk(sk), skb);
 	ret = 0;
