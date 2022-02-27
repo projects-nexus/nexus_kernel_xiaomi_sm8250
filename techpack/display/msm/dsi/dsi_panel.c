30104a9b6bfca381a4a6175f93e83477ca507eff
NotZeetaa <rodrigo2005contente@gmail.com>
2022-02-27 18:40:54 +0000

    Revert "drm: msm: refactor dsi_panel_get_fod_dim_alpha"
    This reverts commit fb8159803676f33dcd0febc8a094b861fa0e802e.

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -796,27 +796,24 @@ static uint32_t interpolate(uint32_t x, uint32_t xa, uint32_t xb, uint32_t ya, u
 uint32_t dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel)
 {
 	u32 brightness = dsi_panel_get_backlight(panel);
-	int i, alpha;
+	int i;
 
 	if (!panel->fod_dim_lut)
-		alpha = 0;
+		return 0;
 
 	for (i = 0; i < panel->fod_dim_lut_count; i++)
 		if (panel->fod_dim_lut[i].brightness >= brightness)
 			break;
 
 	if (i == 0)
-		alpha = panel->fod_dim_lut[0].alpha;
+		return panel->fod_dim_lut[i].alpha;
 
-	else if (i == panel->fod_dim_lut_count)
-		alpha = panel->fod_dim_lut[brightness - 1].alpha;
-	else
-		alpha = interpolate(brightness,
-				panel->fod_dim_lut[i - 1].brightness,
-				panel->fod_dim_lut[i].brightness,
-				panel->fod_dim_lut[i - 1].alpha,
-				panel->fod_dim_lut[i].alpha);
-	return alpha;
+	if (i == panel->fod_dim_lut_count)
+		return panel->fod_dim_lut[i - 1].alpha;
+
+	return interpolate(brightness,
+			panel->fod_dim_lut[i - 1].brightness, panel->fod_dim_lut[i].brightness,
+			panel->fod_dim_lut[i - 1].alpha, panel->fod_dim_lut[i].alpha);
 }
 
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
