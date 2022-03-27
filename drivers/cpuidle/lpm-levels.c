2dd66161c8bae3db43e9bc06df9dce5f4554f3d2
Alexander Winkowski <dereference23@outlook.com>
2022-03-27 12:23:38 +0000

    Revert "drivers: lpm-levels: Use cpu_do_idle for shallower LPM entry"
    This reverts commit eab4ca12f6766b65a578c0c348559874eba85f99.
    
    The DSB instruction is called through rmb() in the do_idle function.
    
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -606,7 +606,7 @@ static int lpm_cpuidle_enter(struct cpuidle_device *dev,
 		goto exit;
 
 	cpuidle_set_idle_cpu(dev->cpu);
-	cpu_do_idle();
+	wfi();
 	success = true;
 	cpuidle_clear_idle_cpu(dev->cpu);
 
