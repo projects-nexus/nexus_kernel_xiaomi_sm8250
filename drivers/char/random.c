33efc5db2eedd8d0ef33fb7853972a96895b4a11
Jason A. Donenfeld <Jason@zx2c4.com>
2022-06-08 10:31:25 +0200

    BACKPORT: random: remove rng_has_arch_random()
    With arch randomness being used by every distro and enabled in
    defconfigs, the distinction between rng_has_arch_random() and
    rng_is_initialized() is now rather small. In fact, the places where they
    differ are now places where paranoid users and system builders really
    don't want arch randomness to be used, in which case we should respect
    that choice, or places where arch randomness is known to be broken, in
    which case that choice is all the more important. So this commit just
    removes the function and its one user.
    
    Reviewed-by: Petr Mladek <pmladek@suse.com> # for vsprintf.c
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -706,7 +706,6 @@ static void __cold _credit_init_bits(size_t bits)
  *
  **********************************************************************/
 
-static bool used_arch_random;
 static bool trust_cpu __initdata = IS_ENABLED(CONFIG_RANDOM_TRUST_CPU);
 static bool trust_bootloader __initdata = IS_ENABLED(CONFIG_RANDOM_TRUST_BOOTLOADER);
 static int __init parse_trust_cpu(char *arg)
@@ -757,20 +756,8 @@ int __init random_init(const char *command_line)
 		crng_reseed();
 	else if (trust_cpu)
 		_credit_init_bits(arch_bits);
-	used_arch_random = arch_bytes * 8 >= POOL_READY_BITS;
 
 	return 0;
-}
-
-/*
- * Returns whether arch randomness has been mixed into the initial
- * state of the RNG, regardless of whether or not that randomness
- * was credited. Knowing this is only good for a very limited set
- * of uses, such as early init printk pointer obfuscation.
- */
-bool rng_has_arch_random(void)
-{
-	return used_arch_random;
 }
 
 /*
