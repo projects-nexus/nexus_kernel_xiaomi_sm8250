009fd1ec97909a232bc7c49dc4018d2807cfe65b
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-06-17 12:07:10 +0000

    Revert "ANDROID: GKI: PM / devfreq: Introduce a sysfs lock"
    This reverts commit 8f43993c585c9c5093b96f06de6d5806bee0907a.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -587,7 +587,6 @@ static void devfreq_dev_release(struct device *dev)
 		devfreq->profile->exit(devfreq->dev.parent);
 
 	mutex_destroy(&devfreq->lock);
-	mutex_destroy(&devfreq->sysfs_lock);
 	kfree(devfreq);
 }
 
@@ -631,7 +630,6 @@ struct devfreq *devfreq_add_device(struct device *dev,
 	}
 
 	mutex_init(&devfreq->lock);
-	mutex_init(&devfreq->sysfs_lock);
 	mutex_lock(&devfreq->lock);
 	devfreq->dev.parent = dev;
 	devfreq->dev.class = devfreq_class;
@@ -1064,13 +1062,12 @@ static ssize_t governor_store(struct device *dev, struct device_attribute *attr,
 		goto out;
 	}
 
-	mutex_lock(&df->sysfs_lock);
 	if (df->governor) {
 		ret = df->governor->event_handler(df, DEVFREQ_GOV_STOP, NULL);
 		if (ret) {
 			dev_warn(dev, "%s: Governor %s not stopped(%d)\n",
 				 __func__, df->governor->name, ret);
-			goto gov_stop_out;
+			goto out;
 		}
 	}
 	df->governor = governor;
@@ -1079,9 +1076,6 @@ static ssize_t governor_store(struct device *dev, struct device_attribute *attr,
 	if (ret)
 		dev_warn(dev, "%s: Governor %s not started(%d)\n",
 			 __func__, df->governor->name, ret);
-
-gov_stop_out:
-	mutex_unlock(&df->sysfs_lock);
 out:
 	mutex_unlock(&devfreq_list_lock);
 
@@ -1176,10 +1170,8 @@ static ssize_t polling_interval_store(struct device *dev,
 	if (ret != 1)
 		return -EINVAL;
 
-	mutex_lock(&df->sysfs_lock);
 	df->governor->event_handler(df, DEVFREQ_GOV_INTERVAL, &value);
 	ret = count;
-	mutex_unlock(&df->sysfs_lock);
 
 	return ret;
 }
@@ -1200,7 +1192,6 @@ static ssize_t min_freq_store(struct device *dev, struct device_attribute *attr,
 	if (ret != 1)
 		return -EINVAL;
 
-	mutex_lock(&df->sysfs_lock);
 	mutex_lock(&df->lock);
 
 	if (value) {
@@ -1223,7 +1214,6 @@ static ssize_t min_freq_store(struct device *dev, struct device_attribute *attr,
 	ret = count;
 unlock:
 	mutex_unlock(&df->lock);
-	mutex_unlock(&df->sysfs_lock);
 	return ret;
 }
 
@@ -1246,7 +1236,6 @@ static ssize_t __maybe_unused max_freq_store(struct device *dev, struct device_a
 	if (ret != 1)
 		return -EINVAL;
 
-	mutex_lock(&df->sysfs_lock);
 	mutex_lock(&df->lock);
 
 	if (value) {
@@ -1269,7 +1258,6 @@ static ssize_t __maybe_unused max_freq_store(struct device *dev, struct device_a
 	ret = count;
 unlock:
 	mutex_unlock(&df->lock);
-	mutex_unlock(&df->sysfs_lock);
 	return ret;
 }
 static DEVICE_ATTR_RW(min_freq);
