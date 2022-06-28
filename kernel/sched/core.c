e69aa4e5243a091d187e4ac06b790fc311ac09ff
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-06-28 19:24:00 +0600

    sched/core: Remove unused `cpu` variable at scheduler_ipi()
    This fixes the following warning:
    
    ../kernel/sched/core.c:2409:6: warning: unused variable 'cpu' [-Wunused-variable]
            int cpu = smp_processor_id();
                ^
    
    Fixes: d27c6b8c9add ("Revert "sched: Fix race condition with active balance"")
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2546,7 +2546,6 @@ void sched_ttwu_pending(void)
 
 void scheduler_ipi(void)
 {
-	int cpu = smp_processor_id();
 	/*
 	 * Fold TIF_NEED_RESCHED into the preempt_count; anybody setting
 	 * TIF_NEED_RESCHED remotely (for the first time) will also send
