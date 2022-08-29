f1e9c14adf0456b6ca209232befacc7150dbd468
NotZeetaa <rodrigo2005contente@gmail.com>
2022-08-29 20:14:28 +0100

    drivers: scsi: ufs: Don't increase Query request timeout
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -228,11 +228,7 @@ static void ufshcd_update_uic_error_cnt(struct ufs_hba *hba, u32 reg, int type)
 /* Query request retries */
 #define QUERY_REQ_RETRIES 3
 /* Query request timeout */
-#ifdef CONFIG_MACH_XIAOMI_SM8250
-#define QUERY_REQ_TIMEOUT 3000 /* 3.0 seconds */
-#else
 #define QUERY_REQ_TIMEOUT 1500 /* 1.5 seconds */
-#endif
 
 /* Task management command timeout */
 #define TM_CMD_TIMEOUT	100 /* msecs */
