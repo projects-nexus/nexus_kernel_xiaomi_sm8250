06c3ffaa49d069cf05a3af3782a80f4d551c9f8c
NotZeetaa <rodrigo2005contente@gmail.com>
2022-09-10 18:11:29 +0000

    Revert "sched: walt: hardcode 4.19 backport values"
    This reverts commit d154da0e84213a63b7fa002d5b378b2e3a879271.
    
    * Power usage increased by alot

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -491,7 +491,7 @@ static struct ctl_table kern_table[] = {
 		.procname	= "sched_min_task_util_for_colocation",
 		.data		= &sysctl_sched_min_task_util_for_colocation,
 		.maxlen		= sizeof(unsigned int),
-		.mode		= 0444,
+		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
 		.extra2		= &one_thousand,
@@ -509,7 +509,7 @@ static struct ctl_table kern_table[] = {
 		.procname	= "sched_coloc_downmigrate_ns",
 		.data		= &sysctl_sched_coloc_downmigrate_ns,
 		.maxlen		= sizeof(unsigned int),
-		.mode		= 0444,
+		.mode		= 0644,
 		.proc_handler	= proc_douintvec_minmax,
 	},
 	{
