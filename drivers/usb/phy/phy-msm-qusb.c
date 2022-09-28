52d7f80f767cd9b6b0bd2938acbe75ec31c77cf1
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-09-28 19:05:08 +0600

    usb: phy: Do not write to little-endian register during EFUSE check
    - Imported from jasmine-q-oss.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/usb/phy/phy-msm-qusb.c
+++ drivers/usb/phy/phy-msm-qusb.c
@@ -586,8 +586,10 @@ static int qusb_phy_init(struct usb_phy *phy)
 
 		pr_debug("%s(): Programming TUNE2 parameter as:%x\n", __func__,
 				qphy->tune2_val);
+#ifndef CONFIG_MACH_LONGCHEER
 		writel_relaxed(qphy->tune2_val,
 				qphy->base + QUSB2PHY_PORT_TUNE2);
+#endif
 	}
 
 	/* If tune modparam set, override tune value */
