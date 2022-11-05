f3600b0cc469d026d8b6835f91745effa4d2ab8a
NotZeetaa <rodrigo2005contente@gmail.com>
2022-11-05 10:19:17 +0000

    Revert "techpack: display: Add config to enable/disable OSS FOD support"
    This reverts commit a290fe8fcf8a6012370105949701c06da926674c.
    
    * No miui support

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -828,7 +828,6 @@ bool dc_skip_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 	}
 }
 
-#ifdef CONFIG_OSSFOD
 static u32 dsi_panel_get_backlight(struct dsi_panel *panel)
 {
 	return panel->bl_config.bl_level;
@@ -880,7 +879,6 @@ int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 
 	return rc;
 }
-#endif
 
 int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 {
@@ -2767,7 +2765,6 @@ error:
 	return rc;
 }
 
-#ifdef CONFIG_OSSFOD
 static int dsi_panel_parse_fod_dim_lut(struct dsi_panel *panel,
 		struct dsi_parser_utils *utils)
 {
@@ -2829,7 +2826,6 @@ count_fail:
 	}
 	return rc;
 }
-#endif
 
 static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 {
@@ -2927,11 +2923,9 @@ static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 	panel->bl_config.bl_inverted_dbv = utils->read_bool(utils->data,
 		"qcom,mdss-dsi-bl-inverted-dbv");
 
-#ifdef CONFIG_OSSFOD
 	rc = dsi_panel_parse_fod_dim_lut(panel, utils);
 	if (rc)
 		pr_err("[%s failed to parse fod dim lut\n", panel->name);
-#endif
 
 	if (panel->bl_config.type == DSI_BACKLIGHT_PWM) {
 		rc = dsi_panel_parse_bl_pwm_config(panel);
