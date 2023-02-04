d2e47798981c573e221062d05c5cc7d5975e184b
NotZeetaa <rodrigo2005contente@gmail.com>
2023-02-04 10:48:31 +0000

    configs: Bump to `rc-1.0`
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- build.sh
+++ build.sh
@@ -66,7 +66,7 @@ TM=$(date +"%F%S")
 
 # Specify Final Zip Name
 ZIPNAME=Nexus
-FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-RC3.0-KERNEL-AOSP-${TM}.zip
+FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-RC1.0-KERNEL-AOSP-${TM}.zip
 
 ##----------------------------------------------------------##
 # Specify compiler [ proton, atomx, eva, aosp ]
