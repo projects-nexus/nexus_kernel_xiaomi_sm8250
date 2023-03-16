d35d7c15f307d99a383c00b4eddf275d809cfe04
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-03-17 03:58:16 +0600

    Revert "msm: adsprpc: Fix race condition in internal_control"
    The mutex placements are incorrect in this patch. The patch is
    also incomplete. LA.UM.11.2.1.r1-03300-sdm660.0 CLO tag has merged
    this commit. So, we are going to import that and drop this one.
    
    This reverts commit 80935f290081e7f04e0c8aec89f77545ec8f95d2.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -475,7 +475,6 @@ struct fastrpc_file {
 	struct mutex perf_mutex;
 	struct pm_qos_request pm_qos_req;
 	int qos_request;
-	struct mutex pm_qos_mutex;
 	struct mutex map_mutex;
 	struct mutex internal_map_mutex;
 	/* Identifies the device (MINOR_NUM_DEV / MINOR_NUM_SECURE_DEV) */
@@ -3880,7 +3879,6 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 	mutex_unlock(&fl->perf_mutex);
 	mutex_destroy(&fl->perf_mutex);
 	mutex_destroy(&fl->map_mutex);
-	mutex_destroy(&fl->pm_qos_mutex);
 	mutex_destroy(&fl->internal_map_mutex);
 	mutex_destroy(&fl->pm_qos_mutex);
 	kfree(fl);
@@ -4256,7 +4254,6 @@ static int fastrpc_device_open(struct inode *inode, struct file *filp)
 	hlist_add_head(&fl->hn, &me->drivers);
 	spin_unlock(&me->hlock);
 	mutex_init(&fl->perf_mutex);
-	mutex_init(&fl->pm_qos_mutex);
 	init_completion(&fl->shutdown);
 	return 0;
 }
