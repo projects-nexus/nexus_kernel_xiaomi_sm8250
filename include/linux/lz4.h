a191d183426c851eeae3a7e160abe4b14abc27eb
NotZeetaa <rodrigo2005contente@gmail.com>
2023-04-09 18:47:25 +0100

    Revert "lz4: Compile LZ4_decompress_safe_partial() when EROFS_FS is enabled"
    This reverts commit 5662fcca4562cdd3d282206ec2a1aed19602ab9d.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -296,7 +296,7 @@ int LZ4_decompress_fast(const char *source, char *dest, int originalSize);
 int LZ4_decompress_safe(const char *source, char *dest, int compressedSize,
 	int maxDecompressedSize);
 
-#if IS_ENABLED(CONFIG_EROFS_FS)
+#if 0
 /**
  * LZ4_decompress_safe_partial() - Decompress a block of size 'compressedSize'
  *	at position 'source' into buffer 'dest'
@@ -322,7 +322,7 @@ int LZ4_decompress_safe(const char *source, char *dest, int compressedSize,
  *	or a negative result in case of error
  *
  */
-int LZ4_decompress_safe_partial(const char *source, char *dest,
+static int LZ4_decompress_safe_partial(const char *source, char *dest,
 	int compressedSize, int targetOutputSize, int maxDecompressedSize);
 #endif
 
