b0011a81698337c419a308c9b3808877d63c1830
NotZeetaa <rodrigo2005contente@gmail.com>
2023-04-09 18:52:50 +0100

    Revert "LZ4: Port LZ4 1.9.x FAST_DEC_LOOP and enable it on x86 and ARM64"
    This reverts commit 0d34b0377ab76315687b32cfa6e736eae2e4ad4b.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- lib/lz4/lz4_compress.c
+++ lib/lz4/lz4_compress.c
@@ -322,7 +322,7 @@ static FORCE_INLINE int LZ4_compress_generic(
 				*token = (BYTE)(litLength << ML_BITS);
 
 			/* Copy Literals */
-			LZ4_wildCopy8(op, anchor, op + litLength);
+			LZ4_wildCopy(op, anchor, op + litLength);
 			op += litLength;
 		}
 
@@ -627,7 +627,7 @@ static int LZ4_compress_destSize_generic(
 				*token = (BYTE)(litLength << ML_BITS);
 
 			/* Copy Literals */
-			LZ4_wildCopy8(op, anchor, op + litLength);
+			LZ4_wildCopy(op, anchor, op + litLength);
 			op += litLength;
 		}
 
