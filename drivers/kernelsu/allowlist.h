d2f46725ce5d649a1f67673f63d5aa568f816431
NotZeetaa <rodrigo2005contente@gmail.com>
2023-06-06 17:39:11 +0000

    drivers: kernelsu: Update KernelSU `(v0.6.0)`
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/kernelsu/allowlist.h
+++ drivers/kernelsu/allowlist.h
@@ -2,6 +2,7 @@
 #define __KSU_H_ALLOWLIST
 
 #include "linux/types.h"
+#include "ksu.h"
 
 void ksu_allowlist_init(void);
 
@@ -13,10 +14,13 @@ void ksu_show_allow_list(void);
 
 bool ksu_is_allow_uid(uid_t uid);
 
-bool ksu_allow_uid(uid_t uid, bool allow, bool persist);
-
 bool ksu_get_allow_list(int *array, int *length, bool allow);
 
 void ksu_prune_allowlist(bool (*is_uid_exist)(uid_t, void *), void *data);
 
+bool ksu_get_app_profile(struct app_profile *);
+bool ksu_set_app_profile(struct app_profile *, bool persist);
+
+bool ksu_uid_should_umount(uid_t uid);
+struct root_profile *ksu_get_root_profile(uid_t uid);
 #endif
