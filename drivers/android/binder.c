2d4a19bfacb4321a9113570a9459695d3f1e75c4
NotZeetaa <rodrigo2005contente@gmail.com>
2023-06-19 20:32:09 +0000

    Revert "BACKPORT: ANDROID: binder: fix pending prio state for early exit"
    This reverts commit 01837548e2e56f64e5352f9fe1a2d08cbf276a47.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -686,13 +686,8 @@ static void binder_do_set_priority(struct binder_thread *thread,
 	bool has_cap_nice;
 	unsigned int policy = desired->sched_policy;
 
-	if (task->policy == policy && task->normal_prio == desired->prio) {
-		spin_lock(&thread->prio_lock);
-		if (thread->prio_state == BINDER_PRIO_PENDING)
-			thread->prio_state = BINDER_PRIO_SET;
-		spin_unlock(&thread->prio_lock);
+	if (task->policy == policy && task->normal_prio == desired->prio)
 		return;
-	}
 
 	has_cap_nice = has_capability_noaudit(task, CAP_SYS_NICE);
 
