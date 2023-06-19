e60cfe457620781e59010e45681ae4e59abf21e4
NotZeetaa <rodrigo2005contente@gmail.com>
2023-06-19 20:43:52 +0000

    Revert "binder: Return EFAULT on failing copy_from_user()"
    This reverts commit dced14a9cdf6aad6266633b2932dde7fc7eab66a.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -4965,7 +4965,7 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 
 		if (copy_from_user(&max_threads, ubuf,
 				   sizeof(max_threads))) {
-			ret = -EFAULT;
+			ret = -EINVAL;
 			goto err;
 		}
 		binder_inner_proc_lock(proc);
@@ -4977,7 +4977,7 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		struct flat_binder_object fbo;
 
 		if (copy_from_user(&fbo, ubuf, sizeof(fbo))) {
-			ret = -EFAULT;
+			ret = -EINVAL;
 			goto err;
 		}
 		ret = binder_ioctl_set_ctx_mgr(filp, &fbo);
