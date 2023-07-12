79eaee2c2d25096e2c9cb964faa3f0ec3cab74ab
utziacre <utziacre@gmail.com>
2023-07-12 15:14:38 +0800

    Revert "workqueue: fix possible livelock with concurrent mod_delayed_work()"
    This reverts commit df0a8b635b9b1a91a13c56424f7b9289495e6bc2.

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -49,8 +49,6 @@
 #include <linux/uaccess.h>
 #include <linux/sched/isolation.h>
 #include <linux/nmi.h>
-#include <linux/bug.h>
-#include <linux/delay.h>
 #include <linux/kvm_para.h>
 
 #include "workqueue_internal.h"
@@ -1310,12 +1308,6 @@ fail:
 	if (work_is_canceling(work))
 		return -ENOENT;
 	cpu_relax();
-	/*
-	 * The queueing is in progress in another context. If we keep
-	 * taking the pool->lock in a busy loop, the other context may
-	 * never get the lock. Give 1 usec delay to avoid this contention.
-	 */
-	udelay(1);
 	return -EAGAIN;
 }
 
