282503801f34045ca0b84bf483c73be1507c15a9
NotZeetaa <rodrigo2005contente@gmail.com>
2023-07-16 09:50:47 +0000

    drivers: kernelsu: Update KernelSU `(0.6.2)`
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/kernelsu/ksu.h
+++ drivers/kernelsu/ksu.h
@@ -4,8 +4,7 @@
 #include "linux/types.h"
 #include "linux/workqueue.h"
 
-#define KERNEL_SU_VERSION (11084)
-
+#define KERNEL_SU_VERSION (11110)
 #define KERNEL_SU_OPTION 0xDEADBEEF
 
 #define CMD_GRANT_ROOT 0
