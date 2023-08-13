4cf8fe4815a6192c7ece0c532b4e649e07259288
NotZeetaa <rodrigo2005contente@gmail.com>
2023-08-13 09:16:52 +0100

    [SQUASH]: mm: Revert SLMK
    * mm clean up moment
    
    This reverts commit 5098309b8fae2cf5a5229ba1d5db9c3a21cf0104.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,7 +95,6 @@
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
-#include <linux/simple_lmk.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1065,7 +1064,6 @@ static inline void __mmput(struct mm_struct *mm)
 	ksm_exit(mm);
 	khugepaged_exit(mm); /* must run before exit_mmap */
 	exit_mmap(mm);
-	simple_lmk_mm_freed(mm);
 	mm_put_huge_zero_page(mm);
 	set_mm_exe_file(mm, NULL);
 	if (!list_empty(&mm->mmlist)) {
