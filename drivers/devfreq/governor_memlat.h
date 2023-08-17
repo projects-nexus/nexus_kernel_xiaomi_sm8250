bdc988ac67532d89f17bfc53d8757754a3edf095
utziacre <utziacre@gmail.com>
2023-08-18 00:41:03 +0800

    Revert "PM / devfreq: memlat: Aggregate memlat mons under a controller"
    This reverts commit 95d9471f412a36752885c8aadfe39c14228a8eb4.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/devfreq/governor_memlat.h
+++ drivers/devfreq/governor_memlat.h
@@ -55,8 +55,6 @@ struct memlat_hwmon {
 	void (*stop_hwmon)(struct memlat_hwmon *hw);
 	unsigned long (*get_cnt)(struct memlat_hwmon *hw);
 	struct device_node *(*get_child_of_node)(struct device *dev);
-	void (*request_update_ms)(struct memlat_hwmon *hw,
-				  unsigned int update_ms);
 	struct device *dev;
 	struct device_node *of_node;
 
@@ -65,7 +63,6 @@ struct memlat_hwmon {
 
 	struct devfreq *df;
 	struct core_dev_map *freq_map;
-	bool should_ignore_df_monitor;
 };
 
 #ifdef CONFIG_DEVFREQ_GOV_MEMLAT
