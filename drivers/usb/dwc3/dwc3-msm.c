4dcb38d0248987143f59b6187af8dcda27f224c2
NotZeetaa <rodrigo2005contente@gmail.com>
2023-09-17 13:49:45 +0100

    tree: Fix prototype errors
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -4495,9 +4495,6 @@ static int dwc3_otg_start_host(struct dwc3_msm *mdwc, int on)
 		schedule_delayed_work(&mdwc->perf_vote_work,
 				msecs_to_jiffies(1000 * PM_QOS_SAMPLE_SEC));
 
-		if (!has_dp_flag)
-			ps5169_cfg_usb();
-
 	} else {
 		dev_dbg(mdwc->dev, "%s: turn off host\n", __func__);
 
@@ -4617,9 +4614,6 @@ static int dwc3_otg_start_peripheral(struct dwc3_msm *mdwc, int on)
 		msm_dwc3_perf_vote_update(mdwc, true);
 		schedule_delayed_work(&mdwc->perf_vote_work,
 				msecs_to_jiffies(1000 * PM_QOS_SAMPLE_SEC));
-
-		if (!has_dp_flag)
-			ps5169_cfg_usb();
 
 	} else {
 		dev_dbg(mdwc->dev, "%s: turn off gadget %s\n",
