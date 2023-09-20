4d56f079ba177537e9edaaef2e77b7ed35e6f5f7
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2023-09-20 15:00:24 +0200

    sched/debug: Remove the /proc/sys/kernel/sched_child_runs_first sysctl
    The /proc/sys/kernel/sched_child_runs_first knob is no longer connected since:
    
       5e963f2bd4654 ("sched/fair: Commit to EEVDF")
    
    Remove it.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20230920130025.412071-2-bigeasy@linutronix.de

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -374,13 +374,6 @@ static int max_extfrag_threshold = 1000;
 #endif
 
 static struct ctl_table kern_table[] = {
-	{
-		.procname	= "sched_child_runs_first",
-		.data		= &sysctl_sched_child_runs_first,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec,
-	},
 #if defined(CONFIG_PREEMPT_TRACER) && defined(CONFIG_PREEMPTIRQ_EVENTS)
 	{
 		.procname       = "preemptoff_tracing_threshold_ns",
