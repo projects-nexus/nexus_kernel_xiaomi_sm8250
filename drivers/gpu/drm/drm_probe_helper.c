56cb8e7a8902194373b4ebf90e67b9af51f164c0
NotZeetaa <rodrigo2005contente@gmail.com>
2023-11-25 09:44:40 +0000

    Revert "gpu: drm: Use power efficient workingqueues"
    This reverts commit 5d3c72a3639aba81d20b65030c2320905a42423f.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/gpu/drm/drm_probe_helper.c
+++ drivers/gpu/drm/drm_probe_helper.c
@@ -253,8 +253,7 @@ void drm_kms_helper_poll_enable(struct drm_device *dev)
 	}
 
 	if (poll)
-		queue_delayed_work(system_power_efficient_wq, 
-							&dev->mode_config.output_poll_work, delay);
+		schedule_delayed_work(&dev->mode_config.output_poll_work, delay);
 }
 EXPORT_SYMBOL(drm_kms_helper_poll_enable);
 
@@ -460,8 +459,9 @@ retry:
 		 */
 		dev->mode_config.delayed_event = true;
 		if (dev->mode_config.poll_enabled)
-			queue_delayed_work(system_power_efficient_wq, 
-								&dev->mode_config.output_poll_work, 0);
+			mod_delayed_work(system_wq,
+					 &dev->mode_config.output_poll_work,
+					 0);
 	}
 
 	/* Re-enable polling in case the global poll config changed. */
@@ -660,8 +660,7 @@ out:
 		drm_kms_helper_hotplug_event(dev);
 
 	if (repoll)
-		queue_delayed_work(system_power_efficient_wq, delayed_work, 
-							DRM_OUTPUT_POLL_PERIOD);
+		schedule_delayed_work(delayed_work, DRM_OUTPUT_POLL_PERIOD);
 }
 
 /**
