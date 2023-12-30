7886dd2ce5681b8e7abaaafa79e49437c81446f5
NotZeetaa <rodrigo2005contente@gmail.com>
2023-12-30 12:34:39 +0000

    Revert "ion: Rewrite to improve clarity and performance"
    This reverts commit b190a8956a52d85c9aebeb69f293e0c6aa940341.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -56,30 +56,20 @@ static bool pool_refill_ok(struct ion_page_pool *pool)
 
 static inline struct page *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 {
-	struct page *page;
-
 	if (fatal_signal_pending(current))
 		return NULL;
-
-	page = alloc_pages(pool->gfp_mask, pool->order);
-	if (page) {
-		mod_node_page_state(page_pgdat(page), NR_ION_HEAP,
-				    1 << pool->order);
-		mm_event_count(MM_KERN_ALLOC, 1 << pool->order);
-	}
-	return page;
+	return alloc_pages(pool->gfp_mask, pool->order);
 }
 
 static void ion_page_pool_free_pages(struct ion_page_pool *pool,
 				     struct page *page)
 {
 	__free_pages(page, pool->order);
-	mod_node_page_state(page_pgdat(page), NR_ION_HEAP, -(1 << pool->order));
 }
 
 static void ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 {
-	spin_lock(&pool->lock);
+	mutex_lock(&pool->mutex);
 	if (PageHighMem(page)) {
 		list_add_tail(&page->lru, &pool->high_items);
 		pool->high_count++;
@@ -92,9 +82,7 @@ static void ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 	nr_total_pages += 1 << pool->order;
 	mod_node_page_state(page_pgdat(page), NR_KERNEL_MISC_RECLAIMABLE,
 							1 << pool->order);
-	mod_node_page_state(page_pgdat(page), NR_ION_HEAP_POOL,
-			    (1 << pool->order));
-	spin_unlock(&pool->lock);
+	mutex_unlock(&pool->mutex);
 }
 
 void ion_page_pool_refill(struct ion_page_pool *pool)
@@ -115,8 +103,6 @@ void ion_page_pool_refill(struct ion_page_pool *pool)
 			ion_pages_sync_for_device(dev, page,
 						  PAGE_SIZE << pool->order,
 						  DMA_BIDIRECTIONAL);
-		mod_node_page_state(page_pgdat(page), NR_ION_HEAP,
-				    1 << pool->order);
 		ion_page_pool_add(pool, page);
 	}
 }
@@ -140,8 +126,6 @@ static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 	nr_total_pages -= 1 << pool->order;
 	mod_node_page_state(page_pgdat(page), NR_KERNEL_MISC_RECLAIMABLE,
 							-(1 << pool->order));
-	mod_node_page_state(page_pgdat(page), NR_ION_HEAP_POOL,
-			    -(1 << pool->order));
 	return page;
 }
 
@@ -154,12 +138,12 @@ struct page *ion_page_pool_alloc(struct ion_page_pool *pool, bool *from_pool)
 	if (fatal_signal_pending(current))
 		return ERR_PTR(-EINTR);
 
-	if (*from_pool && spin_trylock(&pool->lock)) {
+	if (*from_pool && mutex_trylock(&pool->mutex)) {
 		if (pool->high_count)
 			page = ion_page_pool_remove(pool, true);
 		else if (pool->low_count)
 			page = ion_page_pool_remove(pool, false);
-		spin_unlock(&pool->lock);
+		mutex_unlock(&pool->mutex);
 	}
 	if (!page) {
 		page = ion_page_pool_alloc_pages(pool);
@@ -181,12 +165,12 @@ struct page *ion_page_pool_alloc_pool_only(struct ion_page_pool *pool)
 	if (!pool)
 		return ERR_PTR(-EINVAL);
 
-	if (spin_trylock(&pool->lock)) {
+	if (mutex_trylock(&pool->mutex)) {
 		if (pool->high_count)
 			page = ion_page_pool_remove(pool, true);
 		else if (pool->low_count)
 			page = ion_page_pool_remove(pool, false);
-		spin_unlock(&pool->lock);
+		mutex_unlock(&pool->mutex);
 	}
 
 	if (!page)
@@ -241,16 +225,16 @@ int ion_page_pool_shrink(struct ion_page_pool *pool, gfp_t gfp_mask,
 	while (freed < nr_to_scan) {
 		struct page *page;
 
-		spin_lock(&pool->lock);
+		mutex_lock(&pool->mutex);
 		if (pool->low_count) {
 			page = ion_page_pool_remove(pool, false);
 		} else if (high && pool->high_count) {
 			page = ion_page_pool_remove(pool, true);
 		} else {
-			spin_unlock(&pool->lock);
+			mutex_unlock(&pool->mutex);
 			break;
 		}
-		spin_unlock(&pool->lock);
+		mutex_unlock(&pool->mutex);
 		ion_page_pool_free_pages(pool, page);
 		freed += (1 << pool->order);
 	}
@@ -269,7 +253,7 @@ struct ion_page_pool *ion_page_pool_create(gfp_t gfp_mask, unsigned int order,
 	INIT_LIST_HEAD(&pool->high_items);
 	pool->gfp_mask = gfp_mask;
 	pool->order = order;
-	spin_lock_init(&pool->lock);
+	mutex_init(&pool->mutex);
 	plist_node_init(&pool->list, order);
 	if (cached)
 		pool->cached = true;
