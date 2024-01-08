759e27f1680943fbf9de64d2207c4525766f27da
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2024-01-09 03:26:28 +0600

    Revert "rcu: Remove spurious instrumentation_end() in rcu_nmi_enter()"
    We have reverted commit "rcu: Fixup noinstr warnings" which breaks
    the functionality of this patch. So, revert this.
    
    This reverts commit d578396411597a375fd7eb6a4dd415daa2c56823.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -1016,6 +1016,7 @@ noinstr void rcu_nmi_enter(void)
 	} else if (!in_nmi()) {
 		instrumentation_begin();
 		rcu_irq_enter_check_tick();
+		instrumentation_end();
 	}
 	instrumentation_begin();
 	trace_rcu_dyntick(incby == 1 ? TPS("Endirq") : TPS("++="),
