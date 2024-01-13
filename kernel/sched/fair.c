c6aa16c9675770d08e9a6e8886051ce4aefbb3e3
NotZeetaa <rodrigo2005contente@gmail.com>
2024-01-13 09:05:49 +0000

    Revert "Revert "sched/fair: Improve the scheduler""
    This reverts commit 07e86d2fa37ab51a0b46d2f2081158cb2629ae19.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7086,7 +7086,7 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 	unsigned long best_active_cuml_util = ULONG_MAX;
 	unsigned long best_idle_cuml_util = ULONG_MAX;
 	bool prefer_idle = schedtune_prefer_idle(p);
-	bool boosted = fbt_env->boosted;
+	bool boosted;
 	/* Initialise with deepest possible cstate (INT_MAX) */
 	unsigned long best_idle_util = ULONG_MAX;
 	int shallowest_idle_cstate = INT_MAX;
@@ -7117,7 +7117,7 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 	 * case we initialise target_capacity to 0.
 	 */
 	prefer_idle = uclamp_latency_sensitive(p);
-	boosted = uclamp_boosted(p);
+	boosted = fbt_env->boosted || uclamp_boosted(p);
 	if (prefer_idle && boosted)
 		target_capacity = 0;
 
