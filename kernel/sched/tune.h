4be24edb9a4582cd80bdcc91e6be6dac076de2a7
NotZeetaa <rodrigo2005contente@gmail.com>
2024-01-13 09:07:33 +0000

    Revert "kernel: sched: Mitigate non-boosted tasks preempting boosted tasks"
    This reverts commit ee4377b980f58614a13c3db4184aac6a99d76d3e.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -14,7 +14,6 @@ struct target_nrg {
 
 int schedtune_cpu_boost_with(int cpu, struct task_struct *p);
 int schedtune_task_boost(struct task_struct *tsk);
-int schedtune_task_boost_rcu_locked(struct task_struct *tsk);
 
 int schedtune_prefer_idle(struct task_struct *tsk);
 
