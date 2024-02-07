7b61a7f5a7ea3778adfdc1d3cc47c433510499d2
farizmaul <nickshade88@gmail.com>
2024-02-07 15:43:31 +0700

    techpack:camera-xiaomi:cam_irq_controller fix misleading indentation ../techpack/camera-xiaomi/drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.c: In function 'cam_sensor_init_subdev_do_ioctl': ../techpack/camera-xiaomi/drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.c:65:17: warning: this 'if' clause does not guard... [-Wmisleading-indentation]    65 |                 if (rc < 0)       |                 ^~ ../techpack/camera-xiaomi/drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.c:67:25: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'if'    67 |                         break;       |                         ^~~~~
    Signed-off-by: farizmaul <nickshade88@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- techpack/camera-xiaomi/drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.c
+++ techpack/camera-xiaomi/drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.c
@@ -62,7 +62,7 @@ static long cam_sensor_init_subdev_do_ioctl(struct v4l2_subdev *sd,
 	switch (cmd) {
 	case VIDIOC_CAM_CONTROL:
 		rc = cam_sensor_subdev_ioctl(sd, cmd, &cmd_data);
-		if (rc < 0)
+			if (rc < 0)
 			CAM_ERR(CAM_SENSOR, "cam_sensor_subdev_ioctl failed");
 			break;
 	default:
